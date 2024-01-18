45efedc57b0b15fbc66fe7e42ecbfdb82d635667
ToastedToast <hey@toasted.dev>
2024-01-18 14:44:05 +0800

    chore(packages): remove ui package, init scizor package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,42 @@
-// This configuration only applies to the package manager root.
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./packages/*/tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    // indent: ["error", 2],
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+    "quote-props": ["error", "as-needed"],
+    "prefer-const": "error",
+    "no-var": "error",
+    "no-async-promise-executor": "off",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
