850af0933f698d871a51c55f528b086e5c9e1833
ToastedToast <hey@toasted.dev>
2024-03-15 15:08:13 +0800

    add node support to hono template in create-scizor

--- packages/create-scizor/template/hono/typescript/src/index.ts
+++ packages/create-scizor/template/hono/typescript/src/index.ts
@@ -1,7 +1,21 @@
 import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 import { createRouter } from "scizor/hono";
 
 const app = new Hono();
 createRouter(app);
+
+// @ts-expect-error Bun is undefined in the node runtime
+if (typeof Bun === "undefined") {
+  serve(
+    {
+      fetch: app.fetch,
+      port: 3000,
+    },
+    ({ port }) => {
+      console.log("Listening on port %s.", port);
+    },
+  );
+}
 
 export default app;
