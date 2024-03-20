759066febb4af4107aab18612a104729b68cfdc2
ToastedToast <hey@toasted.dev>
2024-03-20 16:53:05 +0800

    feat: add `GetRouteType` type (#15)

--- packages/scizor/src/express/types/route.ts
+++ packages/scizor/src/express/types/route.ts
@@ -1,2 +1,10 @@
-import type { Handler } from "express";
+import type { Handler, Request, Response } from "express";
+
 export type Route = Handler;
+
+export type GetRouteType<Route extends Handler> = Route extends (
+  req: Request,
+  res: Response,
+) => Response<any, infer T>
+  ? T
+  : never;
--- packages/scizor/src/hono/types/route.ts
+++ packages/scizor/src/hono/types/route.ts
@@ -1,2 +1,10 @@
-import type { Handler } from "hono/types";
+import type { Context } from "hono";
+import type { Handler, TypedResponse } from "hono/types";
+
 export type Route = Handler;
+
+export type GetRouteType<Route extends Handler> = Route extends (
+  c: Context,
+) => Response & TypedResponse<infer T>
+  ? T
+  : never;
